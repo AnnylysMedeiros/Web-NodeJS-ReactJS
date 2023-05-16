87a63141e8ca226078e6129245d32e8df25bdaea
Annylys Stiegelmeier Medeiros <annylys.medeiros@boxgroup.net.br>
2023-05-15 23:32:49 -0300

    first commit frontend

---
+++ Projeto-Tarefa/frontend/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Tasks from "./pages/Tasks";
+import TasksForm from "./pages/Tasks/Form";
+import TasksDetail from "./pages/Tasks/Detail";
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tarefas" element={<Tasks />} />
+      <Route path="/tarefas_cadastro" element={<TasksForm />} />
+      <Route path="/tarefas_cadastro/:id" element={<TasksForm />} />
+      <Route path="/tarefas/:id" element={<TasksDetail />} />
+    </Routes>
+  );
+};
+export default App;
